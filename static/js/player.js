766527311472a7dc194573a0e52069c8ad181040
Mark Pariente <markpariente@gmail.com>
2013-06-23 20:56:02 -0700

    Display 'Empty' as player name when no player present
    Fixes #16 in hanggammon/hanggammon

--- static/js/player.js
+++ static/js/player.js
@@ -89,15 +89,33 @@ function playerStateToDisplay()
    var board1TopDiv = document.getElementById('board1TopSeatName');
    var board1BottomDiv = document.getElementById('board1BottomSeatName');
 
+   var team0Player1 = gameState[getPlayerNameKey(1, 0)];
+   var team1Player1 = gameState[getPlayerNameKey(0, 0)];
+   var team0Player2 = gameState[getPlayerNameKey(1, 1)];
+   var team1Player2 = gameState[getPlayerNameKey(0, 1)];
+
+   if (team0Player1 == undefined || team0Player1 == "undefined") {
+      team0Player1 = "Empty";
+   }
+   if (team1Player1 == undefined || team1Player1 == "undefined") {
+      team1Player1 = "Empty";
+   }
+   if (team0Player2 == undefined || team0Player2 == "undefined") {
+      team0Player2 = "Empty";
+   }
+   if (team1Player2 == undefined || team1Player2 == "undefined") {
+      team1Player2 = "Empty";
+   }
+
    if (!boardFlipped) {
-      board0TopDiv.innerHTML = wrapTextWithTeamColors(1, gameState[getPlayerNameKey(1, 0)]);
-      board0BottomDiv.innerHTML = wrapTextWithTeamColors(0, gameState[getPlayerNameKey(0, 0)]);
-      board1TopDiv.innerHTML = wrapTextWithTeamColors(1, gameState[getPlayerNameKey(1, 1)]);
-      board1BottomDiv.innerHTML = wrapTextWithTeamColors(0, gameState[getPlayerNameKey(0, 1)]);
+      board0TopDiv.innerHTML = wrapTextWithTeamColors(1, team0Player1);
+      board0BottomDiv.innerHTML = wrapTextWithTeamColors(0, team1Player1);
+      board1TopDiv.innerHTML = wrapTextWithTeamColors(1, team0Player2);
+      board1BottomDiv.innerHTML = wrapTextWithTeamColors(0, team1Player2);
    } else {
-      board0TopDiv.innerHTML = wrapTextWithTeamColors(0, gameState[getPlayerNameKey(0, 0)]);
-      board0BottomDiv.innerHTML = wrapTextWithTeamColors(1, gameState[getPlayerNameKey(1, 0)]);
-      board1TopDiv.innerHTML = wrapTextWithTeamColors(0, gameState[getPlayerNameKey(0, 1)]);
-      board1BottomDiv.innerHTML = wrapTextWithTeamColors(1, gameState[getPlayerNameKey(1, 1)]);
+      board0TopDiv.innerHTML = wrapTextWithTeamColors(0, team1Player1);
+      board0BottomDiv.innerHTML = wrapTextWithTeamColors(1, team0Player1);
+      board1TopDiv.innerHTML = wrapTextWithTeamColors(0, team1Player2);
+      board1BottomDiv.innerHTML = wrapTextWithTeamColors(1, team0Player2);
    }
 }
